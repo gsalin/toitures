d4ff506d767e15563aa8d9a0b9c7f07ebde51d6e
Guillaume Salin <gsalin@gmail.com>
2017-04-24 18:19:26 +0200

    beginning of app ok

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,47 @@
+class UsersController < ApplicationController
+
+  before_action :set_user, only: [:show, :edit, :update]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  # def new
+  #   @user = User.new
+  # end
+
+  # def create
+  #   @user = User.new(user_params)
+  #   @user.save
+
+  #   redirect_to users_path
+  # end
+
+
+  def edit
+     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+
+    redirect_to user_path(@user)
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:company, :first_name, :last_name, :position, :mobile_phone, :office_phone, :email, :address, :description)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+end
