f58379d76605004625f98c6ab72fa19ceeaa7f8d
Guillaume Salin <gsalin@gmail.com>
2017-05-03 16:19:10 +0200

    skills ok sur show user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,7 @@
 class User < ApplicationRecord
   has_attachment :photo_presentation
   has_attachment  :photo_company_logo
-  has_attachments :photos_projet_1, maximum: 3
-  has_attachments :photos_projet_2, maximum: 3
-  has_attachments :photos_projet_3, maximum: 3
-  has_attachments :photos_projet_4, maximum: 3
+  has_many :projects, dependent: :nullify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
