578b587fc4fa1adf8f66d2719e40f6cebfb7dc33
Edouard Castaignet <ecastaignet@hotmail.com>
2017-05-24 18:29:07 +0200

    new articles

---
+++ app/controllers/charpentes_controller.rb
@@ -0,0 +1,19 @@
+class CharpentesController < ApplicationController
+  skip_before_action :authenticate_user!
+  before_action :new_client
+
+  def index
+  end
+
+  def introduction
+  end
+
+  private
+
+  def new_client
+    @client = Client.new
+  end
+
+
+
+end
