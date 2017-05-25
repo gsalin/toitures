187ddc6e9f849cd8a63e68d9d186c7943cc97c44
Guillaume Salin <gsalin@gmail.com>
2017-05-25 15:35:59 +0200

    home better

--- app/assets/javascripts/front/home.js
+++ app/assets/javascripts/front/home.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   $('.carousel').carousel({
     interval: 3500,
     pause: null,
+
   });
   $('form').on('focus', 'input[type=number]', function (e) {
     $(this).on('mousewheel.disableScroll', function (e) {
