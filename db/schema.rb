1f416a1ec61eca488742b89dbf2798321b47ab97
Edouard Castaignet <ecastaignet@hotmail.com>
2017-06-13 18:23:57 +0200

    installing category-article but not working in the form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170613134015) do
+ActiveRecord::Schema.define(version: 20170613152640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 20170613134015) do
     t.datetime "updated_at",   null: false
     t.string   "card_summary"
     t.text     "summary"
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_articles_on_category_id", using: :btree
   end
 
   create_table "attachinary_files", force: :cascade do |t|
@@ -57,8 +59,6 @@ ActiveRecord::Schema.define(version: 20170613134015) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "article_id"
-    t.index ["article_id"], name: "index_categories_on_article_id", using: :btree
   end
 
   create_table "clients", force: :cascade do |t|
@@ -140,5 +140,5 @@ ActiveRecord::Schema.define(version: 20170613134015) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  add_foreign_key "categories", "articles"
+  add_foreign_key "articles", "categories"
 end
