dc0d929edfac89c2cb7bf13bc259feaa5e76fa79
Guillaume Salin <gsalin@gmail.com>
2017-06-15 10:24:36 +0200

    devise ok excepté pour traduction

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
   Rails.application.routes.draw do
-  devise_for :workers
   mount Attachinary::Engine => "/attachinary"
 
   ActiveAdmin.routes(self)
+  devise_for :workers
   devise_for :users
   root to: 'pages#home'
   resources :users
