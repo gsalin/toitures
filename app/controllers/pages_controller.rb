71e8ac4dbad5db778413fb47602abac86475f9ff
Edouard Castaignet <ecastaignet@hotmail.com>
2017-06-18 20:55:09 +0200

    cgu & mentions légales done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
 
-skip_before_action :authenticate_user!, only: :home
-skip_before_action :authenticate_worker!, only: :home
+skip_before_action :authenticate_user!
+skip_before_action :authenticate_worker!
 
   def home
     @client = Client.new
