40ca70c45aa1ef0824ab226ac85dbdea18ea75a7
Guillaume Salin <gsalin@gmail.com>
2017-06-30 15:19:35 +0200

    ok signup devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  skip_before_action :authenticate_user!, only: :home
 
   def default_url_options
     { host: ENV['HOST'] || 'localhost:3000' }
