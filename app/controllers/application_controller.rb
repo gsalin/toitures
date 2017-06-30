b1a1fc319e2f318e00bb18fdfd3f452ca5e818c4
Guillaume Salin <gsalin@gmail.com>
2017-06-30 14:43:08 +0200

    dead

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  skip_before_action :authenticate_user!, only: :home
 
   def default_url_options
     { host: ENV['HOST'] || 'localhost:3000' }
@@ -9,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :company, :position, :office_phone, :mobile_phone, :address, :description, :radius, :photo_company_logo, :photo_presentation, :zip_code, :city])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :status])
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :company, :position, :office_phone, :mobile_phone, :address, :description, :radius, :photo_company_logo, :photo_presentation, :construction, :renovation, :entretien, :charpente, :couverture, :ouverture, :terrasse, :plomberie, :maison, :chateau, :immeuble, :zip_code, :city, :mh, :rge, :qualibat])
