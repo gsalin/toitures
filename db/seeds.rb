981c3ba9a0a5df3304a78232148a84bfce1f23b8
Edouard Castaignet <ecastaignet@hotmail.com>
2017-07-01 16:15:51 +0200

    article commented dans la seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,12 +28,12 @@ toitindustriel = "https://static.pexels.com/photos/221047/pexels-photo-221047.jp
 ouvrierportrait = "https://static.pexels.com/photos/92033/pexels-photo-92033.jpeg"
 logo = "https://static.pexels.com/photos/430205/pexels-photo-430205.jpeg"
 
-
 require 'faker'
 
 User.destroy_all
 Client.destroy_all
 Category.destroy_all
+# Article.destroy_all
 
 puts 'Creating categories...'
 energie = Category.create!(name: 'Energie')
@@ -248,5 +248,55 @@ puts "creating fake workers..."
   user.save!
   user.photo_url = ouvrierportrait
 end
+
+# puts 'Creating articles...'
+
+# article = Article.new(
+#     title: "Les burons de l’Aubrac et du Cantal",
+#     card_summary: "Témoins de l'occupation paysanne des monts de l'Aubrac et du Cantal, les burons du Massif Central retrouvent une nouvelle vie grâce à l'essor du tourisme vert.",
+#     summary: "Les burons sont de petites maisons en pierre au toit de lauze, édifiées à partir du milieu du XVIIIème siècle dans les monts du Cantal, du Cézallier et de l’Aubrac, où les bergers trouvaient refuge et fabriquaient le fromage pendant les estives. Emblèmes du patrimoine architectural et rural de leurs pays, les burons font l’objet de restaurations et retrouvent  de nouvelles activités économiques grâce au tourisme.",
+#     photo_presentation: File.new("app/assets/images/article/buron.jpg")
+#     second_photo: File.new("app/assets/images/article/buron.jpg"),
+#     category: "patrimoine",
+#     body: "Histoire
+# Issu du vieux français « buiron » qui veut dire cabane, les premiers burons apparaissent vers 1760 pour remplacer les cabanes d’estives des pays du Cantal et de l’Aubrac, les tras (ce qui veut dire trou ou creu), construites sous terre et recouvertes de mottes de gazon. A la fin du XIXème, les burons sont devenus de solides constructions, parfois en partie enterrées, couvertes par toit de lauzes. Dans les années 1945, près d’un millier de buron étaient encore en activité. Le buron est habité par plusieurs buronniers dont les charges varient de la garde du cheptel à la confection des fromages, réservé au Cantalès, le patron-vacher du Buron. Ils furent progressivement abandonnés jusqu’aux débuts des années 2000 où les derniers buronniers arrêtèrent leur activité.
+
+# La difficile sauvegarde des burons
+# Parce que les burons sont isolés aux milieux des montagnes, que leur rénovation est complexe et coûteuse due à la difficulté de rénover un toit en pierres de lauze, la plupart des burons sont aujourd’hui tombés en ruine. La couverture en lauze, taillée dans des blocs de pierre de 40 kg, peut atteindre jusqu’à 250 kg au mètre carré et demande donc une charpente lourde. Le lauze demande également un entretien annuel car les lauzes, soumises aux climats rudes des montagnes, bougent légèrement et doivent donc être recalées correctement après chaque hiver.
+# Encouragées par le soutien de particuliers et d’institutions, deux associations luttent désormais pour la sauvegarde des burons :
+# l’association de sauvegarde des burons du Cantal http://www.burons-du-cantal.fr/
+# L’association «Les Amis de Notre-Dame d’Aubrac »
+
+# La nouvelle vie des burons
+# Grâce à l’essor du tourisme vert sur les hauts plateaux du Massif Centra, une trentaine de burons ont repris une activité et ont été reconvertis en gîtes, refuges de randonneurs, restaurants ou en musée comme le buron de Belles-aigues qui retrace la vie des buronniers du Cantal.
+
+# Certains ont même repris leur activité d’antan en produisant les différents fromages de la région : salers, cantal, tome fraiche, aligot.    Ils sont ouverts pendant la belle saison et vous proposeront de découvrir la traite des vaches et la création des fromages sans oublier leur dégustation.
+# Le Buron d'Algour du col de Néronne chez Guy Chambon, Cantal
+# Le Buron du Col de Legal chez Jean-Paul Ausset, Cantal
+# Le Buron de Caméjane à Saint-Chély d’Aubrac, Aveyron, http://www.buron-en-aubrac.com/index.php
+
+# Se restaurer
+
+# EN LOZERE
+# Buron de Born
+# http://www.lozere-tourisme.com/restaurants/RESLAR048FS000SJ/detail/marchastel/buron-de-born#bloc-presentation)
+# Le Buron du Bes
+# http://www.lozere-tourisme.com/restaurants/RESLAR048FS000UP/detail/recoules-d-aubrac/buron-du-bes
+# Le buron du Che
+# http://www.lozere-tourisme.com/restaurants/RESLAR048FS000RP/detail/nasbinals/buron-du-che
+# EN AVEYRON
+# Le Buron de l’Aubrac
+# https://www.restaurant-buron-aubrac.com/
+
+# Dormir :
+# Dormir dans un buron « Le Buron de Cap Combattut »
+
+# Le buron de Camejane
+# http://www.buron-en-aubrac.com/sistre.php
+# ",
+
+#   )
+#   article.save!
+# end
 
 puts 'Finished!'
