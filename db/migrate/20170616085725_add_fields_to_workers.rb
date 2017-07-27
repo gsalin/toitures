36509f67d1a2393c4920606a2b5dcc947a1af4fd
Guillaume Salin <gsalin@gmail.com>
2017-07-27 19:08:19 +0200

    suppr migrations workers

--- db/migrate/20170616085725_add_fields_to_workers.rb
+++
@@ -1,9 +0,0 @@
-class AddFieldsToWorkers < ActiveRecord::Migration[5.0]
-  def change
-    add_column :workers, :phone_number, :string
-    add_column :workers, :last_name, :string
-    add_column :workers, :first_name, :string
-    add_column :workers, :address, :string
-    add_column :workers, :looking_for_job, :boolean
-  end
-end
--- db/migrate/20170619090058_add_coordinates_to_workers.rb
+++
@@ -1,6 +0,0 @@
-class AddCoordinatesToWorkers < ActiveRecord::Migration[5.0]
-  def change
-    add_column :workers, :latitude, :float
-    add_column :workers, :longitude, :float
-  end
-end
