b14a057d89885c3857e542d5d46e2f080e1e74b0
Edouard Castaignet <ecastaignet@hotmail.com>
2017-09-05 10:20:37 +0200

    added www. to production.rb action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method     = :postmark
   config.action_mailer.postmark_settings   = { api_key: ENV['POSTMARK_API_KEY'] }
-  config.action_mailer.default_url_options = { host: "lestoitures.fr" }
+  config.action_mailer.default_url_options = { host: "www.lestoitures.fr" }
 
   # Settings specified here will take precedence over those in config/application.rb.
 
